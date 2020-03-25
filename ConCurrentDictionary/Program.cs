60b91a4fba1777f79304559a799ac910c72fe1ea
Mohammad Sadegh Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-03-26 00:26:25 +0430

    add some changes

---
+++ ConCurrentDictionary/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Security;
+using System.Threading.Tasks;
+
+namespace ConCurrentDictionary
+{
+    internal static class Program
+    {
+        private static ConcurrentDictionary<string, string> capitals = new ConcurrentDictionary<string, string>();
+
+        private static void AddParis()
+        {
+            var success = capitals.TryAdd("France", "Paris");
+            var who = Task.CurrentId.HasValue ? ("Task " + Task.CurrentId) : "Main Thread";
+
+            Console.WriteLine($"{who} {(success ? "added" : "did not add")} the element.");
+        }
+
+        private static void Main(string[] args)
+        {
+            Task.Factory.StartNew(AddParis);
+            AddParis();
+
+            //capitals["Russia"] = "Leningrad";
+            capitals.AddOrUpdate("Russia", "Moscow",
+                (key, oldValue) => oldValue + " --> Moscow");
+            Console.WriteLine($"The capital of Russia is {capitals["Russia"]}");
+
+            //capitals["Sweden"] = "Uppsala";
+            var capitalOfSweden = capitals.GetOrAdd("Sweden", "Stockholm");
+            Console.WriteLine($"The capital of Sweden is {capitalOfSweden}");
+
+            const string toRemove = "Russia";
+            string removed;
+            var didRemove = capitals.TryRemove(toRemove, out removed);
+            if (didRemove)
+            {
+                Console.WriteLine($"We just remove {removed}");
+            }
+            else
+            {
+                Console.WriteLine($"Failed to remove the capital of {toRemove}");
+            }
+
+
+            Console.WriteLine("Main process has been completed!");
+        }
+    }
+}
---
+++ ReadWriteLock/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ReadWriteLock
+{
+    internal static class Program
+    {
+        private static readonly ReaderWriterLockSlim _padlock = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
+        private static readonly Random _random = new Random();
+
+        private static void Main(string[] args)
+        {
+            //Locker();
+            UpgradableLock();
+
+            Console.WriteLine("Main process has been completed!");
+            Console.ReadKey();
+        }
+
+        private static void Locker()
+        {
+            var x = 0;
+            var tasks = new List<Task>();
+
+            for (var i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    _padlock.EnterReadLock();
+                    Console.WriteLine($"Entered read lock, x = {x}.");
+                    Thread.Sleep(5000);
+                    _padlock.ExitReadLock();
+                    Console.WriteLine($"Exited read lock, x = {x}.");
+                }));
+            }
+
+            try
+            {
+                Task.WaitAll(tasks.ToArray());
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(e =>
+                {
+                    Console.WriteLine(e);
+                    return true;
+                });
+            }
+
+            for (var i = 0; i < 10; i++)
+            {
+                _padlock.EnterWriteLock();
+                Console.WriteLine("Write lock acquired.");
+                x = _random.Next(10);
+                Console.WriteLine($"Set x = {x}");
+                _padlock.ExitWriteLock();
+                Console.WriteLine("Write lock released.");
+            }
+        }
+
+        private static void UpgradableLock()
+        {
+            var x = 0;
+            var tasks = new List<Task>();
+
+            for (var i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    _padlock.EnterUpgradeableReadLock();
+
+                    if (i % 2 == 0)
+                    {
+                        _padlock.EnterWriteLock();
+                        x = _random.Next(10);
+                        _padlock.ExitWriteLock();
+                    }
+
+                    Console.WriteLine($"{i}: Entered read lock, x = {x}.");
+                    Thread.Sleep(1000);
+                    _padlock.ExitUpgradeableReadLock();
+                    Console.WriteLine($"{i}: Exited read lock, x = {x}.");
+                }));
+            }
+
+            try
+            {
+                Task.WaitAll(tasks.ToArray());
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(e =>
+                {
+                    Console.WriteLine(e);
+                    return true;
+                });
+            }
+
+            for (var i = 0; i < 10; i++)
+            {
+                _padlock.EnterWriteLock();
+                Console.WriteLine("Write lock acquired.");
+                x = _random.Next(10);
+                Console.WriteLine($"Set x = {x}");
+                _padlock.ExitWriteLock();
+                Console.WriteLine("Write lock released.");
+            }
+        }
+    }
+}
